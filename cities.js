b077184f4044ee9b33f754e08d227a4b5926ee8e
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-06-22 21:01:19 +0200

    Add more cities

--- cities.js
+++ cities.js
@@ -6,4 +6,9 @@ CITIES =
         {name: 'Leverkusen', latitude: 51.033749, longitude: 6.987290, radius: 3, fillKey: 'city'},
         {name: 'Munich', latitude: 48.135124, longitude: 11.581981, radius: 3, fillKey: 'city'},
         {name: 'Berlin', latitude: 52.520008, longitude: 13.404954, radius: 3, fillKey: 'city'},
+        {name: 'Prague', latitude: 50.075539, longitude: 14.437800, radius: 3, fillKey: 'city'},
+        {name: 'London', latitude: 51.50853, longitude: -0.12574, radius: 3, fillKey: 'city'},
+        {name: 'Leipzig', latitude: 51.333333, longitude: 12.383333, radius: 3, fillKey: 'city'},
+        {name: 'Aachen', latitude: 50.775556, longitude: 6.083611, radius: 3, fillKey: 'city'},
+        {name: 'Moscow', latitude: 55.755833, longitude: 37.617222, radius: 3, fillKey: 'city'},
     ]
