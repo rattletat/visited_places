b8a05b82084c378ab375e8dc59b1d63efe9cd1a4
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-02-17 06:32:25 +0000

    Update locations

--- countries.js
+++ countries.js
@@ -1,15 +1,15 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
-COUNTRIES =
-{
-    DEU: {fillKey: 'visited'},
-    CZE: {fillKey: 'visited'},
-    RUS: {fillKey: 'visited'},
-    CHN: {fillKey: 'visited'},
-    MEX: {fillKey: 'visited'},
-    FRA: {fillKey: 'visited'},
-    ESP: {fillKey: 'visited'},
-    GBR: {fillKey: 'visited'},
-    BGR: {fillKey: 'visited'},
-    NLD: {fillKey: 'visited'},
-    FIN: {fillKey: 'visited'},
-}
+COUNTRIES = {
+  DEU: { fillKey: "visited" },
+  CZE: { fillKey: "visited" },
+  RUS: { fillKey: "visited" },
+  CHN: { fillKey: "visited" },
+  MEX: { fillKey: "visited" },
+  FRA: { fillKey: "visited" },
+  ESP: { fillKey: "visited" },
+  GBR: { fillKey: "visited" },
+  BGR: { fillKey: "visited" },
+  NLD: { fillKey: "visited" },
+  FIN: { fillKey: "visited" },
+  PRT: { fillKey: "visited" },
+};
